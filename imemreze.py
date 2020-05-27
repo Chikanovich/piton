ddf477cdf558d2fa6a9d8046dd07d879726bd46a
Dinko <chikanovich@gmail.com>
2020-05-27 15:06:48 +0200

    v2

--- imemreze.py
+++ imemreze.py
@@ -35,35 +35,23 @@ print(hosts_list)
 print('\n')"""
 
 mydict = {}
-uptime = {}
 
 # Vendor list for MAC address
-scanner.scan(s.NET_ADDR, arguments=s.NMAP_ARGS2 + " --exclude " + s.EXCLUDE_IPS) #ovdje ide NMAP_ARGS2 jer je drugačija vrsta skena, vidi se u settings.py
+scanner.scan(s.NET_ADDR, arguments=s.NMAP_ARGS2 + " --exclude " + s.EXCLUDE_IPS)
 for h in scanner.all_hosts():
     #print(dir(scanner[h]))
     if 'mac' in scanner[h]['addresses']:
         print("IP & MAC:", scanner[h]['addresses'], "\nState:", scanner[h]['status']['state'], "\nHost names:", 
-        scanner[h]['hostnames'], "\nVendor:", scanner[h]['vendor'], "\nUptime:", scanner[h]['uptime']) #uptime izbacuje dictionary sa vrijednostima koliko dugo je uređaj na mreži i kad se bootao
+        scanner[h]['hostnames'], "\nVendor:", scanner[h]['vendor'], "\nUptime:", scanner[h]['uptime'])
         print('------------------------------------------------------')
-
-        #samo provjera što je koja vrsta, nije ni potrebno u kodu
-        """print(isinstance(scanner[h]['addresses'], dict))
-        print(isinstance(scanner[h]['hostnames'], list))
-        print(isinstance(scanner[h]['vendor'], dict))
-        print(isinstance(scanner[h]['uptime'], dict))"""
-
-        scanner[h]['uptime']['seconds'] = int(scanner[h]['uptime']['seconds']) #ovdje sam sekunde parsirao u int za lakši rad s vizualizacijom
+        scanner[h]['uptime']['seconds'] = int(scanner[h]['uptime']['seconds'])
         mydict[h] = scanner[h]['vendor'], scanner[h]['uptime']
-        uptime[h] = scanner[h]['uptime']
 
-"""print('\n')
-print(mydict)
+print('\n')
+"""print(mydict)
 print(mydict.keys())
 print(mydict.values())"""
-print('\n')
-print(uptime)
-print(uptime.keys())
-print(uptime.values())
+
 
 
 #lastboot 'str' -> 'date'
--- pymysqltable.py
+++ pymysqltable.py
@@ -7,54 +7,49 @@ cursor = connection.cursor()
 delete_existing_table = "DROP TABLE IF EXISTS stats"
 
 create_table_query = """CREATE TABLE stats (id int(50) not null auto_increment primary key, \
-     ip VARCHAR(20) NOT NULL, seconds int(50) NOT NULL, lastboot VARCHAR(50) NOT NULL)"""
+     ip VARCHAR(20) NOT NULL, name VARCHAR(30) NOT NULL, seconds int(50) NOT NULL, lastboot VARCHAR(50) NOT NULL, MAC VARCHAR(20) NOT NULL)"""
 
 try:
     cursor.execute(delete_existing_table)
     print("Existing table has been deleted.")
     cursor.execute(create_table_query)
-    print("Stats table has been created.")
+    print("New stats table has been created.")
 
-except Exception as e:
-    print("Exception occured: ", e)
+except:
+    connection.rollback()
+    print("Exception occured. Could not create table.")
 
-"""insert = "INSERT INTO stats (ip, seconds, lastboot) \
-   VALUES ('%s', '%d', '%s')" % \
-   (ip, seconds, lastboot)"""
-
-for key, value in im.uptime.items():
-    insert = "INSERT INTO stats (ip, seconds, lastboot) \
-    VALUES ('{}', '{}', '{}')".format(key, value['seconds'], value['lastboot'])
+for key, value in im.mydict.items():
+    (mac, name), = value[0].items()
+    seconds, lastboot = value[1].items()
+    insert = "INSERT INTO stats (ip, name, seconds, lastboot, MAC) \
+    VALUES ('{}', '{}', '{}', '{}', '{}')".format(key, name, seconds[1], lastboot[1], mac)
     try:
         cursor.execute(insert)
         connection.commit()
     except:
         connection.rollback()
+        print("Exception occured. Could not insert data.")
 
-#try:
-#   cursor.execute(insert)
-#   connection.commit()
-#except:
-#   connection.rollback()
 
-#ispis rezultata u terminalu
-"""
-show = SELECT * FROM stats
+print("\n")
 
+#ispis tablice u terminalu
+show = "SELECT * FROM stats"
 try:
-   # Execute the SQL command
    cursor.execute(show)
-   # Fetch all the rows in a list of lists.
    results = cursor.fetchall()
    for row in results:
       id = row[0]
       ip = row[1]
-      seconds = row[2]
-      lastboot = row[3]
-      # Now print fetched result
-      print ("id = %d, ip = %s, seconds = %d, lastboot = %s" % \
-         (id, ip, seconds, lastboot))
+      name = row[2]
+      seconds = row[3]
+      lastboot = row[4]
+      MAC = row[5]
+      print ("id = %d, ip = %s, name = %s, seconds = %d, lastboot = %s, MAC = %s" % \
+         (id, ip, name, seconds, lastboot, MAC))
 except:
-   print ("Error: unable to fetch data")"""
+   print ("Error: unable to fetch data")
+
 
 connection.close()
